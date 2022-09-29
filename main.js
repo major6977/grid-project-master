015b8941aa55243439ba8bc50071033139244112
major6977 <majordav@msn.com>
2022-09-29 10:19:10 -0700

    removed zombie code

--- main.js
+++ main.js
@@ -1,100 +1,79 @@
-
-// function change() {
-//     let i = 0;
-//     let circle1 = document.getElementById("circle");
-//     let color = ;
-//   circle1.style.backgroundColor = color[i];
-//   i = (i + 1) % color.length;
-//   setInterval(change, 1000);
-// }
-// change();
-
 // Using jQuery and JS
 
-
-
 var currentColor = 0;
-function switchColor1() {  
-    var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];  
-    if (currentColor >= colors.length) currentColor = 0;
+function switchColor1() {
+  var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
+  if (currentColor >= colors.length) currentColor = 0;
 
-    $('.circle-1').css('background-color', colors[currentColor++]);
-    setTimeout(function(){ switchColor1(); }, 1000);
+  $(".circle-1").css("background-color", colors[currentColor++]);
+  setTimeout(function () {
+    switchColor1();
+  }, 1000);
 }
 switchColor1();
 
-
-
-
 var currentColor = 0;
-function switchColor2() {   
-    var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"]; 
-    if (currentColor >= colors.length) currentColor = 0;
+function switchColor2() {
+  var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
+  if (currentColor >= colors.length) currentColor = 0;
 
-    $('.circle-2').css('background-color', colors[currentColor++]);
+  $(".circle-2").css("background-color", colors[currentColor++]);
 
-    setTimeout(function(){ switchColor2(); }, 2000); 
-     }
+  setTimeout(function () {
+    switchColor2();
+  }, 2000);
+}
 switchColor2();
 
-
-
 var currentColor = 0;
-function switchColor3() {  
-    var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];  
-    if (currentColor >= colors.length) currentColor = 0;
+function switchColor3() {
+  var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
+  if (currentColor >= colors.length) currentColor = 0;
 
-    $('.circle-3').css('background-color', colors[currentColor++]);
-      setTimeout(function(){ switchColor3(); }, 1000);
-    // setTimeout(switchColor3, 4000);
+  $(".circle-3").css("background-color", colors[currentColor++]);
+  setTimeout(function () {
+    switchColor3();
+  }, 1000);
 }
 switchColor3();
 
 var currentColor = 0;
-function switchColor4() {    
-    var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
-    if (currentColor >= colors.length) currentColor = 0;
-    $('.circle-4').css('background-color', colors[currentColor++]);
-    setTimeout(function(){ switchColor4(); }, 2000);
+function switchColor4() {
+  var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
+  if (currentColor >= colors.length) currentColor = 0;
+  $(".circle-4").css("background-color", colors[currentColor++]);
+  setTimeout(function () {
+    switchColor4();
+  }, 2000);
 }
 switchColor4();
 
-
-// document.getElementById("rotate").window.addEventListener("load" ,function() {
-//     document.getElementsByClassName("square-2").classList.add("rotating")
-// });
-
 //  Flip Function to container-a
 
- const card = document.getElementById("card")
+const card = document.getElementById("card");
 
- card.addEventListener("click",flipCard);
+card.addEventListener("click", flipCard);
 
 function flipCard() {
-    card.classList.toggle("flipCard");
- }
+  card.classList.toggle("flipCard");
+}
 
 //  Flip card3
 
-const card3 = document.getElementById("card3")
+const card3 = document.getElementById("card3");
 
-card3.addEventListener("click",flipCard3);
+card3.addEventListener("click", flipCard3);
 
 function flipCard3() {
-    card3.classList.toggle("flipCard");
- }
+  card3.classList.toggle("flipCard");
+}
 
 //  Flip card4
 
-const card4 = document.getElementById("card4")
+const card4 = document.getElementById("card4");
 
-card4.addEventListener("click",flipCard4);
+card4.addEventListener("click", flipCard4);
 
 function flipCard4() {
-    card4.classList.toggle("flipCard");
- }
-
-
-
-
-
+  card4.classList.toggle("flipCard");
+}
